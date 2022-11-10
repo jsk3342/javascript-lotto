2ea597db6014e2bd20115b587581b4a718085f13
JiSu Kim <85912592+jsk3342@users.noreply.github.com>
2022-11-11 02:51:06 +0900

    feat(makeUserTicket): 구입 금액에 해당하는 로또 발행

--- src/view.js
+++ src/view.js
@@ -4,18 +4,22 @@ const Lotto = require('./Lotto');
 
 class View {
     #console
+    #userTicket
     constructor() {
         this.#console = MissionUtils.Console
+        this.insertMoney();
     }
 
     insertMoney() {
         this.#console.readLine('구입금액을 입력해 주세요.', (userMoney) => {
-            this.makeUserTicket(userMoney);
+            this.#userTicket = this.makeUserTicket(userMoney);
+            console.log(this.#userTicket)
         })
     }
 
     makeUserTicket(userMoney) {
-        
+        const ticket = userMoney / 1000;
+        return ticket;
     }
 
     inputUserNumber() {
