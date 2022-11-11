b880686ba677ad543a9242a8731466b3a5420978
JiSu Kim <85912592+jsk3342@users.noreply.github.com>
2022-11-11 20:25:08 +0900

    feat(setUserTicket): 티켓 수 만큼 로또 번호 정렬하여 유저에게 보여주도록 구현

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const Lotto = require("./Lotto")
-const lotto = new Lotto();
 
 class App {
-  play() { }
+  #userTicket
+
+  play() { 
+    this.insertMoney();
+    
+  }
 
   insertMoney() {
-      this.#console.readLine('구입금액을 입력해 주세요.', (userMoney) => {
-          this.#userTicket = this.makeUserTicket(userMoney);
-          console.log(this.#userTicket)
+      MissionUtils.Console.readLine('구입금액을 입력해 주세요.', (userMoney) => {
+        this.userTicketCount = this.makeUserTicket(userMoney);
+        this.setUserTicket(this.userTicketCount);
       })
   }
 
   makeUserTicket(userMoney) {
     const ticket = userMoney / 1000;
     return ticket;
+  }
+
+  setUserTicket(userTicketCount) {
+  //티켓의 갯수만큼 로또 번호를 발생한다.
+  //로또는 길이가 6이며 1~45까지 중복되지 않는 6개의 숫자이다.
+  //먼저 하나의 배열을 만들고 반환한다.
+    for (let makeTicket = 0; makeTicket < userTicketCount; makeTicket++){
+      const userTicket = MissionUtils.Random.pickUniqueNumbersInRange(1,45,6).sort((a,b) => a-b)
+      MissionUtils.Console.print(userTicket)
+    }
   }
 
   setRank() {
@@ -41,5 +54,8 @@ class App {
 
   }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
